0c29af42be634934f60c640b6f1fb124e93b4806
lucasmeller <lucasmellermoto@gmail.com>
2024-11-03 09:37:50 -0300

    api done, doc needs last check

--- api_pred.py
+++ api_pred.py
@@ -10,9 +10,9 @@ app = Flask(__name__)
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.get_json()
-    input_data = pd.DataFrame([data])
-    prediction = model.predict(input_data)
-    return jsonify({'prediction': int(prediction[0])})
+    input_data = pd.DataFrame(data)
+    predictions = model.predict(input_data)
+    return jsonify({'predictions': predictions.tolist()})
 
 if __name__ == '__main__':
     app.run(debug=True)
